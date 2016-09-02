4210da3b4eb9019b888a2f30f3ebbe3d776e4c53
Daniel Butler <cactusfly@gmail.com>
2016-09-02 16:37:23 +0000

    updated script.js

--- script.js
+++ script.js
@@ -15,10 +15,12 @@ function randomColor() {
   return color;
 }
 
+//this prints the whole thing... somehow
 function print(message) {randomColor
   document.write(message);
 }
 
+//this will create the color orbs on the page 96 times
 for ( var co = 0 ; co < 96;  co += 1) {
   rgbColor = randomColor();
   html += '<div style="background-color:' + rgbColor + '"></div>';
